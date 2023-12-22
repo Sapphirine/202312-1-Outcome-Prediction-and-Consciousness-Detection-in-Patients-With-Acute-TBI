cc5d738c487841326128ca6bf52bcfe90698caeb
Greg Tracy <gregory.tracy@columbia.edu>
2023-12-21 21:40:36 -0800

    Update available synthetic generation techniques

--- Synthetic_Data/generate_synthetic_matlab_file.py
+++ Synthetic_Data/generate_synthetic_matlab_file.py
@@ -3,29 +3,49 @@ import pydicom
 from pydicom.dataset import Dataset, FileMetaDataset
 import numpy as np
 import os
-import glob
+import argparse
+import cv2
 
-mat_file_path = 'image_data.mat'
+# Set up the argument parser
+parser = argparse.ArgumentParser(description="Process DICOM files.")
+parser.add_argument('input_path', type=str, help='Path to the input .mat file')
+parser.add_argument('output_directory', type=str, help='Path to the output directory for DICOM files')
+args = parser.parse_args()
+
+# Use the command line arguments
+mat_file_path = args.input_path
+output_directory = args.output_directory
+
 mat_contents = scipy.io.loadmat(mat_file_path)
 image_data_array = mat_contents['image_data']
-output_directory = 'output_dicom_directory'
+
 if not os.path.exists(output_directory):
     os.makedirs(output_directory)
+
+def create_struct_array(image_data_structs):
+    # Convert the list of structs to a numpy structured array with the same format as the original
+    dtypes = [('Patient_ID', 'O'), ('Data', 'O'), ('Number', 'O'), 
+              ('Designator', 'O'), ('Imaging_Type', 'O'), ('Technique', 'O')]
+    structured_array = np.zeros((len(image_data_structs),), dtype=dtypes)
+    
+    for i, struct in enumerate(image_data_structs):
+        for field in dtypes:
+            value = struct[field[0]]
+            if field[0] in ['Patient_ID', 'Designator', 'Imaging_Type', 'Technique']:
+                # For strings, ensure they are stored as numpy objects
+                structured_array[i][field[0]] = np.array([value], dtype='object')
+            elif field[0] == 'Number':
+                # For numbers, ensure they are stored as numpy integers
+                structured_array[i][field[0]] = np.array([int(value)], dtype='object')
+            else:
+                # For the Data, store the array directly
+                structured_array[i][field[0]] = value
+    
+    return structured_array
 
 def extract_info(struct):
     patient_id = struct['Patient_ID'][0][0]
     image_number = struct['Number'][0][0]
-    if isinstance(patient_id, bytes):
-        patient_id = patient_id.decode('utf-8')
-    elif isinstance(patient_id, np.ndarray):
-        if patient_id.dtype.type is np.bytes_:
-            patient_id = patient_id.item().decode('utf-8')
-        else:
-            patient_id = str(patient_id.item())
-    if isinstance(image_number, np.ndarray):
-        image_number = int(image_number.item())
-    else:
-        image_number = int(image_number)
     return patient_id, image_number
 
 def create_dicom_file(data, patient_id, image_number, output_path):
@@ -54,47 +74,65 @@ def create_dicom_file(data, patient_id, image_number, output_path):
     ds.PixelRepresentation = 0
     pydicom.dcmwrite(output_path, ds)
 
-for index, struct in enumerate(image_data_array[0]):
-    image_data = np.array(struct['Data'], dtype=np.uint16)
-    patient_id, image_number = extract_info(struct)
-    if not patient_id or not image_number:
-        print(f"Skipping index {index} due to invalid patient_id or image_number")
-        continue
-    output_path = os.path.join(output_directory, f'image_{patient_id}_{image_number}.dcm')
-    create_dicom_file(image_data, patient_id, image_number, output_path)
+def apply_synthetic_technique(image, technique):
+    if technique == 'binary_thresholded':
+        _, modified_image = cv2.threshold(image, 127, 255, cv2.THRESH_BINARY)
+    elif technique == 'inverted_colors':
+        modified_image = cv2.bitwise_not(image)
+    elif technique == 'rotated':
+        center = (image.shape[1] // 2, image.shape[0] // 2)
+        rotation_matrix = cv2.getRotationMatrix2D(center, 45, 1)
+        modified_image = cv2.warpAffine(image, rotation_matrix, (image.shape[1], image.shape[0]))
+    elif technique == 'adaptive_thresholded':
+        modified_image = cv2.adaptiveThreshold(image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+                                               cv2.THRESH_BINARY, 11, 2)
+    elif technique == 'colored':
+        modified_image = cv2.applyColorMap(image, cv2.COLORMAP_JET)
+    elif technique == 'equalized':
+        modified_image = cv2.equalizeHist(image)
+    return modified_image
 
-print("DICOM file creation complete.")
+# Define the list of synthetic techniques to apply
+technique_names = ['binary_thresholded', 'inverted_colors', 'rotated',
+                   'adaptive_thresholded', 'colored', 'equalized']
 
-new_image_data_struct = []
+# Process each technique
+for technique_name in technique_names:
+    # This list will store the structs for the new .mat file
+    new_image_data_struct = []
 
-for index, struct in enumerate(image_data_array[0]):
-    patient_id, image_number = extract_info(struct)
-    output_path = os.path.join(output_directory, f'image_{patient_id}_{image_number}.dcm')
-    try:
-        ds = pydicom.dcmread(output_path, force=True)
-    except Exception as e:
-        print(f"Error reading DICOM file {output_path}: {e}")
-        continue
-    image_struct = {
-        'Patient_ID': np.array([patient_id], dtype='object'),
-        'Data': ds.pixel_array,
-        'Number': np.array([image_number], dtype='object'),
-        'Designator': np.array([struct['Designator'][0]], dtype='object'),
-        'Imaging_Type': np.array([struct['Imaging_Type'][0]], dtype='object'),
-        'Technique': np.array([struct['Technique'][0]], dtype='object'),
-    }
-    new_image_data_struct.append(image_struct)
+    # Apply technique to each image and create a new DICOM file
+    for index, struct in enumerate(image_data_array[0]):
+        patient_id, image_number = extract_info(struct)
+        original_output_path = os.path.join(output_directory, f'image_{patient_id}_{image_number}.dcm')
+        
+        try:
+            ds = pydicom.dcmread(original_output_path, force=True)
+            modified_image = apply_synthetic_technique(ds.pixel_array, technique_name)
 
-dtypes = [
-    ('Patient_ID', 'O'), ('Data', 'O'), ('Number', 'O'), 
-    ('Designator', 'O'), ('Imaging_Type', 'O'), ('Technique', 'O')
-]
-new_image_data_array = np.zeros((len(new_image_data_struct),), dtype=dtypes)
-for i, struct in enumerate(new_image_data_struct):
-    for field in dtypes:
-        new_image_data_array[i][field[0]] = struct[field[0]]
+            # Create a new DICOM file for the modified image
+            modified_output_path = os.path.join(output_directory, f'image_{patient_id}_{image_number}_{technique_name}.dcm')
+            create_dicom_file(modified_image, patient_id, image_number, modified_output_path)
 
-new_mat_file_path = 'new_image_data.mat'
-scipy.io.savemat(new_mat_file_path, {'image_data': new_image_data_array})
+            # Append the modified image data to the list for the new .mat file
+            image_struct = {
+                'Patient_ID': np.array([patient_id], dtype='object'),
+                'Data': modified_image,
+                'Number': np.array([image_number], dtype='object'),
+                'Designator': np.array([struct['Designator'][0]], dtype='object'),
+                'Imaging_Type': np.array([struct['Imaging_Type'][0]], dtype='object'),
+                'Technique': np.array([technique_name], dtype='object'),
+            }
+            new_image_data_struct.append(image_struct)
+        
+        except Exception as e:
+            print(f"Error processing index {index} with technique {technique_name}: {e}")
 
-print("New .mat file with DICOM data created.")
+    # Create the structured array with the correct format
+    new_image_data_array = create_struct_array(new_image_data_struct)
+
+    # Save the structured array to a .mat file
+    new_mat_file_path = os.path.join(output_directory, f'new_image_data_{technique_name}.mat')
+    scipy.io.savemat(new_mat_file_path, {'image_data': new_image_data_array})
+
+    print(f"New .mat file with {technique_name} data created.")
